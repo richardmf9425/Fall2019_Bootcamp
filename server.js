60bdcefc0e607568aecfe3624785b11025b1ce97
Richard Mena <richardmf9425@yahoo.com>
2019-09-04 20:40:52 -0400

    upload to github

--- server.js
+++ server.js
@@ -1,51 +1,41 @@
-var http = require('http'), 
-    fs = require('fs'), 
-    url = require('url'),
-    port = 8080;
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const port = 8080;
 
 /* Global variables */
-var listingData, server;
+let listingData;
+let server;
 
-var requestHandler = function(request, response) {
-  var parsedUrl = url.parse(request.url);
-
-  /*
-    Your request handler should send listingData in the JSON format as a response if a GET request 
-    is sent to the '/listings' path. Otherwise, it should send a 404 error. 
-
-    HINT: Explore the request object and its properties 
-    HINT: Explore the response object and its properties
-    https://code.tutsplus.com/tutorials/http-the-protocol-every-web-developer-must-know-part-1--net-31177
-    http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
-    
-    HINT: Explore how callback's work 
-    http://www.theprojectspot.com/tutorial-post/nodejs-for-beginners-callbacks/4
-    
-    HINT: Explore the list of MIME Types
-    https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/MIME_types/Complete_list_of_MIME_types
-   */
+//  router object, more scalable and better for testing
+const router = {
+  '/listings GET': {
+    mime: 'application/json',
+  },
 };
+function requestHandler(request, response) {
+  const parsedUrl = url.parse(request.url);
+  const { method } = request;
+  const route = parsedUrl.pathname;
+  const match = router[`${route} ${method}`];
 
-fs.readFile('listings.json', 'utf8', function(err, data) {
-  /*
-    This callback function should save the data in the listingData variable, 
-    then start the server. 
+  if (!match) {
+    response.writeHead(404).end('Bad gateway error');
+    return;
+  }
 
-    HINT: Check out this resource on fs.readFile
-    //https://nodejs.org/api/fs.html#fs_fs_readfile_path_options_callback
+  response.writeHead(200, { 'Content-Type': match.mime });
+  response.end(JSON.stringify(listingData));
+}
 
-    HINT: Read up on JSON parsing Node.js
-   */
+fs.readFile('listings.json', 'utf8', (err, data) => {
+  if (err) throw err;
 
-    //Check for errors
-  
+  listingData = JSON.parse(data);
 
-   //Save the sate in the listingData variable already defined
-  
-
-  //Creates the server
-  
-  //Start the server
-
-
+  server = http.createServer(requestHandler);
+  server.listen(port, () => {
+    console.log(`server listening on: http://localhost:${port}`);
+  });
 });
