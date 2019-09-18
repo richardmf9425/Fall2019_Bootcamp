2bd9eec5b8590c7a1ac72242c476dbc975e53372
Richard Mena <47158790+richardmf9425@users.noreply.github.com>
2019-09-18 16:43:52 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  db: {
-    uri: 'mongodb+srv://richardm:ufwebapp2019@cluster0-gan1x.mongodb.net/test?retryWrites=true&w=majority', // place the URI of your mongo database here.
-  },
-};
